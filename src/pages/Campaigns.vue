63dc8dfe68fc2ff1bc5a8af4e31fb27fd6a450da
Arthurin915 <arthurindarocha1@hotmail.com>
2019-06-03 14:26:53 -0300

    card de campanha finalizados

--- src/pages/Campaigns.vue
+++ src/pages/Campaigns.vue
@@ -57,7 +57,7 @@
         </div>
       </md-tab>
       <md-tab id="tab-rascunhos" md-label="Rascunhos" md-icon="create">
-        <div class="md-layout md-grutter campaign-container">
+        <!-- <div class="md-layout md-grutter campaign-container">
           <div v-for="(campaign, index) in rascunhos" :key="index" class="md-layout-item card-container md-size-33">
             <md-card>
               <md-card-actions class="overlap-btn">
@@ -97,7 +97,40 @@
               </md-card-actions>
             </md-card>
           </div>
-        </div>
+        </div> -->
+        <section class="cards-container">
+          <div class="card-container" v-for="(campaign, index) in rascunhos" :key="index">
+            <div class="img-container">
+              <img :src="$url +campaign.cover_url" onerror="this.src='https://via.placeholder.com/150'" alt="">
+              <md-button class="md-fab md-mini delete-btn" @click="openDeleteConfirmation(campaign.id)">
+                  <md-icon>delete</md-icon>
+                </md-button>
+            </div>
+            <div class="campaign-body">
+              <div class="campaign-title">
+                <h2>{{campaign.title}}
+                </h2>
+              </div>
+              <div class="campaign-description">
+                <p>{{campaign.description}}
+                </p>
+              </div>
+            </div>
+            <div class="campaign-footer">
+              <router-link :to="'/edit/'+campaign.id">
+                <md-button class="custom-color-1">
+                  <md-icon>edit</md-icon>Editar
+                </md-button>
+              </router-link>
+              <md-button class="custom-color-2">
+                <md-icon>find_in_page</md-icon>Prévia
+              </md-button>
+              <md-button class="custom-color">
+                <md-icon>send</md-icon>Publicar
+              </md-button>
+            </div>
+          </div>
+        </section>
       </md-tab>
       <md-tab id="tab-analise" md-label="Em Análise" md-icon="timeline">
         <div class="md-layout md-grutter">
@@ -193,15 +226,82 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.campaign-container {
-  .card-container {
-    margin-bottom: 1.5rem;
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  // display: grid;
+  // grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  // grid-gap: 20px;
+  @media (max-width: 679px) {
+    justify-content: center;
   }
-  .overlap-btn {
-    position: absolute;
-    right: 0;
+
+  .card-container {
+    box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+    margin-top: 1rem;
+    margin-right: 1rem;
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    // margin-bottom: 1rem;
+    width: 350px;
+    @media (max-width: 330px) {
+      width: 295px;
+      margin-right: 0;
+    }
+    .img-container {
+      position: relative;
+      img {
+        width: 100%;
+        height: 225px;
+      }
+      .delete-btn {
+        position: absolute;
+         right: 0;
+      }
+    }
+    .campaign-body {
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      padding: 1rem;
+      .campaign-title {
+        h2 {
+          font-weight: 500;
+        }
+      }
+      .campaign-description {
+        p {
+          margin: 0;
+        }
+      }
+    }
+    .campaign-footer {
+      display: flex;
+      position: relative;
+      margin-top: auto; 
+      justify-content: center;
+      &::before {
+        position: absolute;
+        content: '';
+        border-bottom: 1px solid rgba(0,0,0,0.12);;
+        width: 90%;
+        transform: translateX(-50%);
+        left: 50%;
+      }
+    }
   }
 }
+
+// .campaign-container {
+//   .card-container {
+//     margin-bottom: 1.5rem;
+//   }
+//   .overlap-btn {
+//     position: absolute;
+//     right: 0;
+//   }
+// }
 
 .loading-overlay {
   z-index: 10;
